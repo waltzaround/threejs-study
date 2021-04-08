62caccbc0354d395b5d797870476b25ab1353d29
Walter Lim <waltissomewhere@gmail.com>
2021-04-08 21:40:19 +1200

    first scene

---
+++ 03-basic-scene/script.js
@@ -0,0 +1,11 @@
+//create scene
+const scene = new THREE.Scene();
+
+//create red cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+//create material
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+
+console.log(scene);
+console.log(THREE);
