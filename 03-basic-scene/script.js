f4905891669fb4ef449acfa903e660ced7db550c
Walter Lim <waltissomewhere@gmail.com>
2021-04-08 22:02:12 +1200

    cube now renders

--- 03-basic-scene/script.js
+++ 03-basic-scene/script.js
@@ -1,11 +1,39 @@
+
 //create scene
 const scene = new THREE.Scene();
 
 //create red cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-//create material
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+// sizes.width = Screen.width;
+// sizes.height = Screen.height;
+//Create the camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+camera.rotation.y = 0.2;
+camera.rotation.x = -0.15;
+camera.rotation.z = -0.15;
+scene.add(camera);
+
+// capture canvas
+const canvas = document.querySelector('canvas.webgl')
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas:  canvas
+})
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
 
 console.log(scene);
 console.log(THREE);
+console.log(sizes);
