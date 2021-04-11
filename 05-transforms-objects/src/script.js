9638caf29b79cd4c0cc2c33085087c99a8d13c46
Walter Lim <waltissomewhere@gmail.com>
2021-04-11 21:27:23 +1200

    rotation

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -14,15 +14,35 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+//Positioning
 // mesh.position.x = 0.5;
 // mesh.position.y = -0.6;
 // mesh.position.z = 1;
+mesh.position.set(0, 1, -1);
 
-mesh.position.set(0, -0.6, 1);
-mesh.rotation.x = 1.25;
-mesh.rotation.y = 1.25;
+//scale
+// mesh.scale.x = 2;
+// mesh.scale.y = 0.25;
+// mesh.scale.z = 0.5;
+mesh.scale.set(2, 0.25, 0.5);
+
+//Rotation
+mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = 1.25;
+// mesh.rotation.y = 1.25;
+// mesh.rotation.z = 0.25;
+mesh.rotation.set(Math.PI * 0.25, Math.PI * 0.25, Math.PI * 0.25);
+
+//quarternion rotation
+
 mesh.position.normalize();
 console.log(mesh.position.length());
+
+// Axes helper
+// const axesHelper = new THREE.AxesHelper(12);
+// scene.add(axesHelper);
+
 /**
  * Sizes
  */
@@ -35,7 +55,9 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 2;
+// camera.position.y = 1;
+// camera.position.x = 1;
 scene.add(camera);
 
 console.log(mesh.position.normalize(camera.position));
