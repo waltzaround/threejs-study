b43efea78347d245077f9e982fe5e81ad93ee811
Walter Lim <waltissomewhere@gmail.com>
2021-04-13 20:17:05 +1200

    gsap

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -41,13 +41,15 @@ const clock = new THREE.Clock();
 
 //smoothing anim
 gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: -2 });
+gsap.to(mesh.position, { duration: 1, delay: 3, x: 0 });
 
 // Animations
 const tick = () => {
-  // // using three builtin time
-  // const elapsedTime = clock.getElapsedTime();
-  // console.log(elapsedTime);
-  // console.log("tick");
+  // using three builtin time
+  const elapsedTime = clock.getElapsedTime();
+  console.log(elapsedTime);
+  console.log("tick");
 
   //   // Time example
   //   const currentTime = Date.now();
@@ -55,10 +57,10 @@ const tick = () => {
   //   time = currentTime;
   //   //   console.log(deltaTime);
 
-  // // update object rotation
-  // mesh.rotation.y = elapsedTime * Math.PI * 0.2;
-  // mesh.rotation.x = elapsedTime * Math.PI * 0.2;
-  // mesh.rotation.z = elapsedTime * Math.PI * 0.2;
+  // update object rotation
+  mesh.rotation.y = elapsedTime * Math.PI * 0.2;
+  mesh.rotation.x = elapsedTime * Math.PI * 0.2;
+  mesh.rotation.z = elapsedTime * Math.PI * 0.2;
 
   // //movement
   // mesh.position.x = Math.cos(elapsedTime);
